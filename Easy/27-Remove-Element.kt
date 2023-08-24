a8dde50c2dca6baa93f404746d5542dbd015bcc7
Panterkoo <75864809+Panterkoo@users.noreply.github.com>
2023-08-24 15:24:02 +0200

    Create 27-Remove-Element.kt

---
+++ Easy/27-Remove-Element.kt
@@ -0,0 +1,29 @@
+// Name: 27. Remove Element
+// Difficulty: Easy
+// Acceptance rate: 54.3%
+// Runtime: Beats 53.88%
+// Memory usage: Beats 33.97%
+// Not proud of this one
+
+class Solution {
+    fun removeElement(nums: IntArray, `val`: Int): Int {
+        var elements = 0 
+        var index = 0
+
+        nums.forEach{if (it != `val`) elements++}
+        
+        for (num in nums.indices) {
+            if (nums[num] == `val`) {
+                while (nums[index] == `val`) {
+                    if (index >= nums.size - 1) break
+                    index++
+                }
+                nums[num] = nums[index]
+                nums[index] = `val`
+                continue
+            }
+            index++
+        }
+        return elements
+    }
+}
