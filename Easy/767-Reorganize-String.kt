f70361bbea39159e4c5e7c9b785c4ba7fed32f60
Tomas Zobac <xzobac@fi.muni.cz>
2023-08-29 22:28:24 +0200

    767 done

--- Easy/767-Reorganize-String.kt
+++ Easy/767-Reorganize-String.kt
@@ -1,56 +1,58 @@
 fun reorganizeString(s: String): String {
+    if (s.length in 0..1) return s
+
     var letters = LinkedHashMap<Char,Int>()
     var organized = charArrayOf()
-    var max = s[0]
+    var max = 'A'
+    var premax = 'A'
 
     for (letter in s) {
         if (letter !in letters.keys) {
             var count = s.count {it == letter}
             letters[letter] = count
-            if (count > letters[max]!!) max = letter
+            if (count > (letters[max] ?: 0)) {
+                premax = max
+                max = letter
+            }
+            if (count <= (letters[max] ?: 0) && count > (letters[premax] ?: 0) && letter != max) premax = letter
         }
     }
 
-    while (letters.isNotEmpty()) {
-        if (letters.count() == 1 && letters[letters.keys.first()]!! > 1) return ""
+    if (max == 'A' || premax == 'A') return ""
+
+    while (letters.values.filter { it > 0 }.isNotEmpty()) {
         organized += max
+        organized += premax
         letters[max] = letters[max]!! - 1
-        var diff = false
-        if (letters[max] == 0) {
-            var temp = findMax(letters, max)
-            if (letters[max] == 0) letters.remove(max)
-            max = temp
-        }
+        letters[premax] = letters[premax]!! - 1
         for (letter in letters.keys) {
-            if (letter != max) {
-                diff = true
-                organized += letter
-                letters[letter] = letters[letter]!! - 1
-                if (letters[letter] == 0) letters.remove(letter)
-                break;
+            if (letters.values.filter { it > 0 }.isEmpty()) return String(organized)
+            if (letters[letter]!! > letters[max]!!) {
+                premax = max
+                max = letter
+            }
+            if (letters[letter]!! > letters[premax]!! && letter != max) {
+                premax = letter
             }
         }
-        //if (!diff) return ""
+        if ((letters[max] == 0 && letters[premax] == 0) || (letters[max] == 1 && letters[premax] == 0)) {
+            organized += max
+            return String(organized)
+        }
+        if ((letters[max] == 0 && letters[premax]!! > 0) || (letters[max]!! > 1 && letters[premax] == 0)) return ""
     }
 
     return String(organized)
 }
 
-fun findMax(map: LinkedHashMap<Char,Int>, max: Char): Char {
-    var newmax = max
-    for (letter in map.keys) {
-        if (map[letter]!! > map[newmax]!!) newmax = letter
-    }
-    return newmax
-}
 
 fun main() {
 
     val long = "gpneqthatplqrofqgwwfmhzxjddhyupnluzkkysofgqawjyrwhfgdpkhiqgkpupgdeonipvptkfqluytogoljiaexrnxckeofqojltdjuujcnjdjohqbrzzzznymyrbbcjjmacdqyhpwtcmmlpjbqictcvjgswqyqcjcribfmyajsodsqicwallszoqkxjsoskxxstdeavavnqnrjelsxxlermaxmlgqaaeuvneovumneazaegtlztlxhihpqbajjwjujyorhldxxbdocklrklgvnoubegjrfrscigsemporrjkiyncugkksedfpuiqzbmwdaagqlxivxawccavcrtelscbewrqaxvhknxpyzdzjuhvoizxkcxuxllbkyyygtqdngpffvdvtivnbnlsurzroxyxcevsojbhjhujqxenhlvlgzcsibcxwomfpyevumljanfpjpyhsqxxnaewknpnuhpeffdvtyjqvvyzjeoctivqwann"
-    println(reorganizeString("aab"))
-    println(reorganizeString("aaa"))
-    println(reorganizeString("aaab"))
-    println(reorganizeString("baaba"))
-    println(reorganizeString("bfrbs"))
+    println("aab:" + reorganizeString("aab"))
+    println("aaa:" + reorganizeString("aaa"))
+    println("aaab:" + reorganizeString("aaab"))
+    println("baaba:" + reorganizeString("baaba"))
+    println("bfrbs:" + reorganizeString("bfrbs"))
     println(reorganizeString(long))
 }
